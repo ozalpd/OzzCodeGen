e3b701c349f09320d7e9c4ab96ec3ae6422adccb
Özalp Döndüren <ozalp@donduren.com>
2015-09-13 23:31:13 +0300

    AppSettins.cs of Localization.Wpf did not transferred in recent commit+push

---
+++ OzzLocalization.Wpf/Models/AppSettings.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OzzUtils.Wpf;
+using OzzUtils.Wpf.Savables;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace OzzLocalization.Wpf.Models
+{
+    public class AppSettings : SavableViewModel
+    {
+        public WindowPosition MainWindowPosition
+        {
+            set { _mainWindowPosition = value; }
+            get
+            {
+                if (_mainWindowPosition == null)
+                {
+                    _mainWindowPosition = new WindowPosition();
+                }
+                return _mainWindowPosition;
+            }
+        }
+        WindowPosition _mainWindowPosition;
+
+
+
+
+
+
+
+
+        public static AppSettings GetAppSettings()
+        {
+            if (_settings == null)
+            {
+                _settings = AppSettings.OpenSettings();
+            }
+            return _settings;
+        }
+        static AppSettings _settings;
+
+        protected static string SettingsFile
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_settingsFile))
+                {
+                    _settingsFile = System.IO.Path.Combine(
+                                System.IO.Path.GetDirectoryName(
+                                System.Reflection.Assembly.GetExecutingAssembly().Location),
+                                "OzzVocabularyEditor.settings");
+                }
+                return _settingsFile;
+            }
+        }
+        static string _settingsFile;
+
+        protected static AppSettings OpenSettings()
+        {
+            if (!File.Exists(SettingsFile))
+            {
+                return new AppSettings();
+            }
+
+            StreamReader reader = new StreamReader(SettingsFile);
+            XmlSerializer x = new XmlSerializer(typeof(AppSettings));
+            AppSettings settings = (AppSettings)x.Deserialize(reader);
+            reader.Close();
+
+            return settings;
+        }
+
+        public virtual void SaveSettings()
+        {
+            StreamWriter writer = new StreamWriter(SettingsFile);
+            XmlSerializer x = new XmlSerializer(this.GetType());
+            x.Serialize(writer, this);
+            writer.Close();
+        }
+    }
+}
